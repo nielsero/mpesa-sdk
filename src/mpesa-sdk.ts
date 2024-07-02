7a9c0bb1d9b99345218d3a0f1ebe15dc0962d152
nielsero <niels.cpp@gmail.com>
2024-07-02 19:42:51 +0200

    feat: Add configuration

--- src/mpesa-sdk.ts
+++ src/mpesa-sdk.ts
@@ -1,1 +1,12 @@
-export function test() {}
+import { Configuration } from "./types";
+
+const urlSandbox = "https://api.sandbox.vm.co.mz";
+const urlProduction = "https://api.vm.co.mz";
+
+const configuration: Configuration = {
+  mode: "sandbox",
+  apiKey: null,
+  publicKey: null,
+  origin: null,
+  serviceProviderCode: null,
+};
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,7 @@
-export type TestProps = {};
+export type Configuration = {
+  mode: "sandbox" | "production";
+  apiKey: string | null;
+  publicKey: string | null;
+  origin: string | null;
+  serviceProviderCode: number | null;
+};
